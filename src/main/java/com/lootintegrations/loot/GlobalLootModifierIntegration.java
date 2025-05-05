59c33212888fe3cec1ee560ea5c4fc7e410313d7
someaddons <someaddons@github.com>
2025-05-05 17:50:32 +0200

    4.4: Add config to skip adding the same item already present in the loot, defaults to true

--- src/main/java/com/lootintegrations/loot/GlobalLootModifierIntegration.java
+++ src/main/java/com/lootintegrations/loot/GlobalLootModifierIntegration.java
@@ -97,10 +97,32 @@ public class GlobalLootModifierIntegration
         for (int i = 0; i < itemCount; i++)
         {
             final ItemStack stack = extraItems.remove(LootintegrationsMod.rand.nextInt(extraItems.size()));
-            generatedLoot.add(stack);
-            if (LootintegrationsMod.config.getCommonConfig().debugOutput)
+
+            boolean sameItem = false;
+
+            if (LootintegrationsMod.config.getCommonConfig().skipExistingItems)
             {
-                LootintegrationsMod.LOGGER.info("Adding loot to: " + ((ILootTableID) lootTable).getID() + " item:" + stack.toString());
+                for (int j = 0; j < generatedLoot.size(); j++)
+                {
+                    if (ItemStack.isSameItemSameTags(generatedLoot.get(j), stack))
+                    {
+                        sameItem = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!sameItem)
+            {
+                generatedLoot.add(stack);
+                if (LootintegrationsMod.config.getCommonConfig().debugOutput)
+                {
+                    LootintegrationsMod.LOGGER.info("Adding loot to: " + ((ILootTableID) lootTable).getID() + " item:" + stack.toString());
+                }
+            }
+            else
+            {
+                i--;
             }
 
             if (extraItems.isEmpty())
