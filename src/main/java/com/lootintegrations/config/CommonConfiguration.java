cb2abdff0324c8f545fe441e619a6259265d81b8
someaddons <someaddons@github.com>
2025-06-09 13:02:58 +0200

    4.7: Improve loot generation for modded items(esp on neoforge/forge) Add new config to allow giving modded items a higher chance of being chosen Fix skip existing item option not appearing in the config

--- src/main/java/com/lootintegrations/config/CommonConfiguration.java
+++ src/main/java/com/lootintegrations/config/CommonConfiguration.java
@@ -8,6 +8,8 @@ public class CommonConfiguration implements ICommonConfig
     public boolean showcontainerloottable = false;
     public boolean debugOutput            = false;
     public boolean skipMapItems = true;
+    public boolean skipExistingItems = false;
+    public int     moddedItemWeight  = 3;
 
     public CommonConfiguration()
     {
@@ -19,7 +21,7 @@ public class CommonConfiguration implements ICommonConfig
         final JsonObject root = new JsonObject();
 
         final JsonObject entry = new JsonObject();
-        entry.addProperty("desc:", "Set to true to print containers loottable to chat on first open: default:false");
+        entry.addProperty("desc:", "Set to true to show containers loottable on first open: default:false");
         entry.addProperty("showcontainerloottable", showcontainerloottable);
         root.add("showcontainerloottable", entry);
 
@@ -27,6 +29,17 @@ public class CommonConfiguration implements ICommonConfig
         entrentry3.addProperty("desc:", "Skips map items during additional item generation, to avoid structure search lag (maps in the original chest still exist): default:true");
         entrentry3.addProperty("skipMapItems", skipMapItems);
         root.add("skipMapItems", entrentry3);
+
+        final JsonObject entry4 = new JsonObject();
+        entry4.addProperty("desc:",
+            "Skips adding items already present in the loot, to avoid stacking additional items. Note that this increases the chance for modded loot aswell. default:false");
+        entry4.addProperty("skipExistingItems", skipExistingItems);
+        root.add("skipExistingItems", entry4);
+
+        final JsonObject entry5 = new JsonObject();
+        entry5.addProperty("desc:", "Add additional weight to modded items (e.g. 10 = 10 times more likely), making them more likely to be chosen default:3, minimum:0");
+        entry5.addProperty("moddedItemWeight", moddedItemWeight);
+        root.add("moddedItemWeight", entry5);
 
         final JsonObject entry2 = new JsonObject();
         entry2.addProperty("desc:", "Shows the added loot in the log if enabled: default:false");
@@ -41,5 +54,7 @@ public class CommonConfiguration implements ICommonConfig
         showcontainerloottable = data.get("showcontainerloottable").getAsJsonObject().get("showcontainerloottable").getAsBoolean();
         debugOutput = data.get("debugOutput").getAsJsonObject().get("debugOutput").getAsBoolean();
         skipMapItems = data.get("skipMapItems").getAsJsonObject().get("skipMapItems").getAsBoolean();
+        skipExistingItems = data.get("skipExistingItems").getAsJsonObject().get("skipExistingItems").getAsBoolean();
+        moddedItemWeight = Math.max(0, data.get("moddedItemWeight").getAsJsonObject().get("moddedItemWeight").getAsInt());
     }
 }
--- src/main/java/com/lootintegrations/loot/GlobalLootModifierIntegration.java
+++ src/main/java/com/lootintegrations/loot/GlobalLootModifierIntegration.java
@@ -10,10 +10,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.LootTable;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class GlobalLootModifierIntegration
 {
@@ -21,6 +18,14 @@ public class GlobalLootModifierIntegration
     public        ResourceLocation               lootTableId;
     public        Map<ResourceLocation, Integer> integratedTables = new HashMap<>();
     private       int                            fillSize         = 27;
+
+    private static final Set<ResourceLocation> inbuiltTables = Set.of(LootintegrationsMod.resFor("chests/easy"),
+        LootintegrationsMod.resFor("chests/medium"),
+        LootintegrationsMod.resFor("chests/hard"),
+        LootintegrationsMod.resFor("chests/nether"),
+        LootintegrationsMod.resFor("chests/water"),
+        LootintegrationsMod.resFor("chests/village"),
+        LootintegrationsMod.resFor("chests/empty"));
 
     /**
      * Constructs a LootModifier.
@@ -57,7 +62,7 @@ public class GlobalLootModifierIntegration
             return;
         }
 
-        if (LootintegrationsMod.config.getCommonConfig().debugOutput)
+        if (LootintegrationsMod.config.getCommonConfig().debugOutput && !inbuiltTables.contains(lootTableId))
         {
             LootintegrationsMod.LOGGER.info("Adding loot to: " + ((ILootTableID) lootTable).getID() + "from: " + lootTableId + " caused by:" + location);
         }
@@ -68,7 +73,7 @@ public class GlobalLootModifierIntegration
         }
 
         int itemCount = integratedTables.getOrDefault(((ILootTableID) lootTable).getID(), 1);
-        extraItems = aggregateStacks(extraItems);
+        extraItems = aggregateStacks(extraItems, false);
 
         if (extraItems.isEmpty())
         {
@@ -77,7 +82,7 @@ public class GlobalLootModifierIntegration
 
         if (!generatedLoot.isEmpty() && (generatedLoot.size() + itemCount) > fillSize)
         {
-            List<ItemStack> newList = aggregateStacks(generatedLoot);
+            List<ItemStack> newList = aggregateStacks(generatedLoot, true);
             generatedLoot.clear();
             generatedLoot.addAll(newList);
             if (generatedLoot.size() > fillSize)
@@ -90,13 +95,77 @@ public class GlobalLootModifierIntegration
             }
         }
 
+        if (itemCount == 0)
+        {
+            return;
+        }
+
+        int[] weights = new int[extraItems.size()];
+        int totalWeight = 0;
+
+        int size = extraItems.size();
+        for (int i = 0; i < size; i++)
+        {
+            int weight = calcWeightForStack(extraItems.get(i));
+            totalWeight += weight;
+            weights[i] = totalWeight;
+        }
+
         for (int i = 0; i < itemCount; i++)
         {
-            final ItemStack stack = extraItems.remove(LootintegrationsMod.rand.nextInt(extraItems.size()));
-            generatedLoot.add(stack);
-            if (LootintegrationsMod.config.getCommonConfig().debugOutput)
+            if (totalWeight <= 0)
             {
-                LootintegrationsMod.LOGGER.info("Adding loot to: " + ((ILootTableID) lootTable).getID() + " item:" + stack.toString());
+                return;
+            }
+
+            int weight = LootintegrationsMod.rand.nextInt(totalWeight);
+            int index = -1;
+            int removedWeight = 0;
+            ItemStack stack = null;
+            for (int j = 0; j < size; j++)
+            {
+                if (index == -1 && weight < weights[j])
+                {
+                    index = j;
+                    weights[j] = 0;
+                    stack = extraItems.get(index);
+                    removedWeight = calcWeightForStack(stack);
+                    totalWeight -= removedWeight;
+                    continue;
+                }
+
+                if (index != -1)
+                {
+                    weights[j] -= removedWeight;
+                }
+            }
+
+            if (stack == null)
+            {
+                continue;
+            }
+
+            boolean sameItem = false;
+
+            if (LootintegrationsMod.config.getCommonConfig().skipExistingItems)
+            {
+                for (int j = 0; j < generatedLoot.size(); j++)
+                {
+                    if (ItemStack.isSameItemSameTags(generatedLoot.get(j), stack))
+                    {
+                        sameItem = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!sameItem)
+            {
+                generatedLoot.add(stack);
+                if (LootintegrationsMod.config.getCommonConfig().debugOutput)
+                {
+                    LootintegrationsMod.LOGGER.info("Adding loot to: " + ((ILootTableID) lootTable).getID() + " item:" + stack.toString());
+                }
             }
 
             if (extraItems.isEmpty())
@@ -107,12 +176,23 @@ public class GlobalLootModifierIntegration
     }
 
     /**
-     * Aggregates the itemstacks in a list together, by item. May remove different variants of the same
-     *
-     * @param stacksIn
+     * Determines the weight for a stack
+     * @param stack
      * @return
      */
-    private List<ItemStack> aggregateStacks(final List<ItemStack> stacksIn)
+    private int calcWeightForStack(final ItemStack stack)
+    {
+        return stack.getItemHolder().unwrapKey().get().location().getNamespace().equals("minecraft") ? 1 : LootintegrationsMod.config.getCommonConfig().moddedItemWeight + 1;
+    }
+
+    /**
+     * Aggregates the itemstacks in a list together, by item
+     *
+     * @param stacksIn
+     * @param originalLoot
+     * @return
+     */
+    private List<ItemStack> aggregateStacks(final List<ItemStack> stacksIn, final boolean originalLoot)
     {
         final Map<Item, ItemStack> aggregated = new HashMap<>();
         for (final ItemStack stack : stacksIn)
@@ -158,7 +238,10 @@ public class GlobalLootModifierIntegration
 
         modifier.lootTableId = new ResourceLocation(jsonData.get(LOOT_TABLE_ID).getAsString());
 
-        LootintegrationsMod.LOGGER.info("Parsing loot modifiers for:" + location + " with loottable: " + modifier.lootTableId);
+        if (LootintegrationsMod.config.getCommonConfig().debugOutput && location.getPath().contains("lootintegrations_"))
+        {
+            LootintegrationsMod.LOGGER.info("Parsing loot modifiers for:" + location + " with loottable: " + modifier.lootTableId);
+        }
 
         if (jsonData.has(MAX_RESULT_ITEMCOUNT))
         {
