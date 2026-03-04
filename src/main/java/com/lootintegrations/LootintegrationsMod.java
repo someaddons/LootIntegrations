f3ec10eef68be991ca75719423baa2fcd79d83ef
someaddons <someaddons@github.com>
2026-03-04 14:18:35 +0100

    4.8: Prevent crash when encountering an unregistered lootable

--- src/main/java/com/lootintegrations/LootintegrationsMod.java
+++ src/main/java/com/lootintegrations/LootintegrationsMod.java
@@ -40,6 +40,12 @@ public class LootintegrationsMod implements ModInitializer
     public static ResourceLocation getLootTableId(final LootTable table, final MinecraftServer server)
     {
         final Identifier id = ((Registry) server.reloadableRegistries().lookup().lookup(Registries.LOOT_TABLE).get()).getKey(table);
+
+        if (id == null)
+        {
+            return null;
+        }
+
         return new ResourceLocation(id.getNamespace(), id.getPath());
     }
 
