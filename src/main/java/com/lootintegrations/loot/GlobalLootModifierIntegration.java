6d420144dc990e3a6e4880c0ee9d0c47f075cc1f
someaddons <someaddons@github.com>
2026-04-11 22:42:22 +0200

    26.1

--- src/main/java/com/lootintegrations/loot/GlobalLootModifierIntegration.java
+++ src/main/java/com/lootintegrations/loot/GlobalLootModifierIntegration.java
@@ -200,7 +200,7 @@ public class GlobalLootModifierIntegration
      */
     private int calcWeightForStack(final ItemStack stack)
     {
-        return stack.getItemHolder().unwrapKey().get().identifier().getNamespace().equals("minecraft") ? 1 : LootintegrationsMod.config.getCommonConfig().moddedItemWeight + 1;
+        return stack.typeHolder().unwrapKey().get().identifier().getNamespace().equals("minecraft") ? 1 : LootintegrationsMod.config.getCommonConfig().moddedItemWeight + 1;
     }
 
     /**
